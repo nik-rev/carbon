7c4b367ad0da1f5f5d0b1b7cfde5d8fe7ef5ce8a
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-10 16:57:47 +0100

    chore(eslint): enable several rules

---
+++ src/types/debug.d.ts
@@ -0,0 +1,22 @@
+/* eslint "@typescript-eslint/ban-types": "off" -- Would otherwise error and flag the two types below */
+
+// These types are for debugging purposes only.
+
+/** Expands a type by one level */
+type Expand<T> = T extends (...args: infer A) => infer R
+  ? (...args: Expand<A>) => Expand<R>
+  : T extends infer O
+    ? { [K in keyof O]: O[K] }
+    : never;
+
+/** Expands a type by an infinite number of levels */
+type ExpandRecursively<T> = T extends (...args: infer A) => infer R
+  ? (...args: ExpandRecursively<A>) => ExpandRecursively<R>
+  : T extends object
+    ? T extends infer O
+      ? { [K in keyof O]: ExpandRecursively<O[K]> }
+      : never
+    : T;
+
+/** Hide implementation details like Omit<T, K> and instead show the object type */
+type Resolve<T> = T extends Function ? T : { [K in keyof T]: T[K] };
