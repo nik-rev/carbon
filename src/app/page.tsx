ad3965f82a14162a8a16b5e8defddd17a61d3f71
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-10 21:54:31 +0100

    chore(eslint): migrate most of the config into eslint.config.mjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+
+/* eslint functional/prefer-immutable-types: "off" */
 export default function Home() {
-  return <div className="-top-[5px]" />;
+  const name = Math.random() > 0.5 ? "hehe" : "";
+  if (name !== "") {
+    useEffect(() => {
+      localStorage.setItem("formData", name);
+    });
+  }
+
+  return (
+    <div className="-top-[5px]">
+      <p />
+
+      <img />
+    </div>
+  );
 }
