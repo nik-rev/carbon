54e5ff96d3064196699a94bf1db586295be6eb01
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-22 22:33:07 +0100

    feat: implement copy button functionality for code blocks

--- src/components/mdx/code.tsx
+++ src/components/mdx/code.tsx
@@ -1,15 +1,58 @@
 "use client";
 
-import { cloneElement } from "react";
+import { Check, Copy } from "lucide-react";
+import { Children, cloneElement, useState } from "react";
 import { type HTMLAttributes } from "react";
 
+const extractTextFromChildren = (childrenOrText, text = "") => {
+  if (childrenOrText === null) {
+    return text;
+  }
+
+  if (typeof childrenOrText === "string") {
+    return extractTextFromChildren(null, text.concat(childrenOrText));
+  }
+
+  if ("props" in childrenOrText && "children" in childrenOrText.props) {
+    return extractTextFromChildren(childrenOrText.props.children, text);
+  }
+
+  if (Array.isArray(childrenOrText)) {
+    return childrenOrText.reduce(
+      (acc, kid) => acc.concat(extractTextFromChildren(kid)),
+      text,
+    );
+  }
+};
+
 export function CodeBlock({ children }: HTMLAttributes<HTMLPreElement>) {
+  const [isCopying, setIsCopying] = useState(false);
+
+  const handleCopy = () => {
+    const text = extractTextFromChildren(children);
+
+    setIsCopying(true);
+
+    void navigator.clipboard.writeText(text);
+
+    setTimeout(() => {
+      setIsCopying(false);
+    }, 3000);
+  };
+
   return (
-    <pre className="-mx-4 my-4 overflow-x-scroll bg-mantle p-4 md:-mx-8 md:my-8 md:p-8">
+    <pre className="relative -mx-4 my-4 overflow-x-scroll bg-mantle p-4 md:-mx-8 md:my-8 md:p-8">
       {
         // @ts-expect-error -- will always be <code> element
         cloneElement(children, { isInCodeBlock: true })
       }
+      <button onClick={handleCopy}>
+        {isCopying ? (
+          <Check className="absolute right-2 top-2 border-surface0 bg-crust" />
+        ) : (
+          <Copy className="absolute right-2 top-2 border-surface0 bg-crust" />
+        )}
+      </button>
     </pre>
   );
 }
