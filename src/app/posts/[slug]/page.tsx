97020363f1e2533e78a783fa3dd737d720a89656
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-23 18:41:44 +0100

    feat: make h1 look prettier

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -23,32 +23,30 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   };
 };
 
-        // <time dateTime={post.date}>
-        //   {format(parseISO(post.date), "LLLL do, yyyy")}
-        // </time>
 function SectionBubble({ post }) {
   return (
-    <div className="relative mb-[30vw] md:mb-[20vw] bg-mantle w-full flex justify-start">
-      <span className="-mb-12 block bg-mantle max-sm:pt-20 pb-24 pt-32 md:pb-20 md:pt-28 max-w-prose mx-auto">
-        <h1 className="scroll-m-20 bg-gradient-to-b from-text to-subtext1/80 bg-clip-text text-left text-4xl font-extrabold tracking-tight text-transparent lg:text-5xl mx-4 md:mx-8">
+    <div className="relative flex w-full flex-col justify-start">
+      <span className="flex w-full flex-col justify-center bg-mantle items-center gap-12">
+        <h1 className="max-w-[656.5px] scroll-m-20 bg-gradient-to-b m-8 from-text to-subtext1 bg-clip-text pt-10 text-left text-4xl font-extrabold tracking-tight text-transparent lg:py-16 lg:text-5xl">
           {post.title}
         </h1>
+        <time dateTime={post.date} className="text-subtext0 mb-8">
+          {format(parseISO(post.date), "LLLL do, yyyy")}
+        </time>
       </span>
 
       {/* Custom Divider */}
-      <div className="absolute bottom-0 h-[20vw] w-full translate-y-full bg-base">
+      <div className="mb-8 w-full overflow-hidden">
         <svg
+          data-name="Layer 1"
+          className="block h-[100px] max-h-[10vw] w-full"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1185 248"
+          viewBox="0 0 1200 120"
           preserveAspectRatio="none"
-          className="size-full"
         >
-          <circle cx="76" cy="121.1" r="20" className="fill-mantle" />
-          <circle cx="870" cy="201.1" r="11" className="fill-mantle" />
-          <circle cx="814.5" cy="165.6" r="24.5" className="fill-mantle" />
           <path
-            d="M0 0v17.7c22.7 14.8 53 31.9 90.7 51.5 150.8 78 322 116.6 424.8 69.3 102.9-47.4 138-69.3 210.8-69.3s118.3 48.6 219.5 38.3 76.3-59.3 188.7-59.3c18.9 0 35.5 2.6 50.5 6.8V0H0z"
-            className="fill-mantle w-full"
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            className="fill-mantle"
           />
         </svg>
       </div>
@@ -70,7 +68,7 @@ function PostLayout({ params }: { readonly params: { slug: string } }) {
   return (
     <div className="flex flex-col items-center">
       <SectionBubble post={post} />
-      <article className="flex max-w-[100vw] flex-col max-md:px-4 sm:max-w-prose z-10">
+      <article className="z-10 flex max-w-[100vw] flex-col max-md:px-4 sm:max-w-prose">
         <MDXContent components={mdxComponents} />
       </article>
     </div>
