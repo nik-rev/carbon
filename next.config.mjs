962be7ce1ca27fba1d314fadfc9806f78abbfb6c
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-01 19:22:08 +0100

    build: configure next.js for deployment to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/blog",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withContentlayer(nextConfig);
