503799b82c56d4f133ae9d04623fcadda9e3c847
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-30 21:08:33 +0000

    feat: add preview images for every project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,41 +1,41 @@
 import { H2 } from "@/components/mdx/heading";
 import { Li, Ol } from "@/components/mdx/lists";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import odinProjects from "./gh-projects";
 
 export default function Projects() {
   return (
-    <div className="flex flex-col gap-24 bg-base">
+    <div className="flex max-w-article flex-col gap-24 self-center bg-base">
       {odinProjects.map((project) => (
-        <Card key={project.name}>
+        <Card key={project.name} className="border-none">
           <CardHeader>
             <CardTitle className="text-4xl">
               {project.name.replaceAll("-", " ")}
             </CardTitle>
-            <CardDescription>
-              {project.finishDate}
-              <Ol>
-                <Li listType="ol">
-                  Live Demo:{" "}
-                  <a href={project.pagesUrl} className="text-green">
-                    {project.pagesUrl}
-                  </a>
-                </Li>
-                <Li listType="ol">
-                  Github Site:{" "}
-                  <a href={project.repoUrl} className="text-green">
-                    {project.repoUrl}
-                  </a>
-                </Li>
-              </Ol>
-            </CardDescription>
+            <time>{project.finishDate}</time>
+            <Ol>
+              <div
+                role="img"
+                aria-label={`preview image for project ${project.name}`}
+                style={{
+                  backgroundImage: `url(/projects/${project.name}.png)`,
+                }}
+                className="size-72 origin-center bg-cover bg-center"
+              />
+              <Li listType="ol">
+                Live Demo:{" "}
+                <a href={project.pagesUrl} className="text-green">
+                  {project.pagesUrl}
+                </a>
+              </Li>
+              <Li listType="ol">
+                Github Site:{" "}
+                <a href={project.repoUrl} className="text-green">
+                  {project.repoUrl}
+                </a>
+              </Li>
+            </Ol>
           </CardHeader>
           <CardContent>
             <H2 className="mt-0 text-xl">Things Learned</H2>
