c657468ce2f35de1dd83ad64ef2595d5e414638c
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-28 23:50:05 +0000

    fix: remove starlight function call

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,20 @@ import expressiveCode from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
-import starlight from "@astrojs/starlight";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [expressiveCode({
-    themes: ["everforest-dark"],
-    plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
-    defaultProps: {
-      showLineNumbers: false,
-    },
-  }), mdx(), starlight()],
+  integrations: [
+    expressiveCode({
+      themes: ["everforest-dark"],
+      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+      defaultProps: {
+        showLineNumbers: false,
+      },
+    }),
+    mdx(),
+  ],
 });
