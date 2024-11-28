a326b13fd2d1be179ec9b9805b5137491487783c
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-28 12:42:37 +0000

    feat: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx()],
+});
