698c2be54e19beda539d3f5caa5767e87870100e
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-11-28 20:34:11 +0000

    feat: add second post, about semantic HTML elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,20 +9,19 @@ import expressiveCode from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [
-    expressiveCode({
-      themes: ["everforest-dark"],
-      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
-      defaultProps: {
-        showLineNumbers: false,
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [expressiveCode({
+    themes: ["everforest-dark"],
+    plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+    defaultProps: {
+      showLineNumbers: false,
+    },
+  }), mdx(), starlight()],
 });
